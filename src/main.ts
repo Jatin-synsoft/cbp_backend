f54fd9b5b5eb9502fc231b9739f2b5d7be119b41
Jatin-synsoft <maheshwarinarsing@gmail.com>
2025-11-18 13:51:52 +0530

    new 6

--- src/main.ts
+++ src/main.ts
@@ -10,22 +10,23 @@ import { ValidationPipe } from '@nestjs/common';
 import * as express from 'express';
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
-    rawBody: true,
-  });
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
   const configService = app.get(ConfigService);
   const reflector = app.get(Reflector);
 
-  // Stripe Webhook MUST use raw body
-  app.use('/stripe/webhook', express.raw({ type: '*/*' }));
-  app.use('/stripe/webhook/platform', express.raw({ type: '*/*' }));
-  // app.use('/stripe/webhook/connect', express.raw({ type: 'application/json' })); // if needed
+  app.use('/stripe/webhook', express.raw({ type: 'application/json' }));
+  app.use('/stripe/webhook/platform', express.raw({ type: 'application/json' }));
 
-  // app.use(express.json());    
+  app.use(express.json());
 
-  app.setGlobalPrefix('api', { exclude: ['/', 'stripe/webhook', 'stripe/webhook/platform'] });
-  app.useGlobalPipes(new ValidationPipe({ transform: true }));
+  app.setGlobalPrefix('api', {
+    exclude: [
+      '/',
+      'stripe/webhook',
+      'stripe/webhook/platform',
+    ],
+  }); app.useGlobalPipes(new ValidationPipe({ transform: true }));
   app.useGlobalInterceptors(new ResponseInterceptor(reflector));
   app.useGlobalFilters(new AllExceptionsFilter());
   app.enableCors();
